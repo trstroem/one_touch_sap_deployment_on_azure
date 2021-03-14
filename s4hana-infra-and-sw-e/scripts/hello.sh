f6650157aed94d3067fa811622ce29422911fb44
trstroem <52523037+trstroem@users.noreply.github.com>
2021-03-14 13:16:41 +0100

    Update hello.sh

--- s4hana-infra-and-sw-e/scripts/hello.sh
+++ s4hana-infra-and-sw-e/scripts/hello.sh
@@ -63,9 +63,9 @@ az login --service-principal --username $2 --password $3 --tenant $4
 bastionRequired=$14
 if [ "$bastionRequired" = "yes" ]
 then
-az deployment group create  --resource-group $5 --template-uri "https://raw.githubusercontent.com/sanjeevkumar761/one_touch_sap_deployment_on_azure/master/s4hana-infra-and-sw-e/azure-bastion-deploy.json" --parameters location=$6 bastionHostName='s4bastion' subnetName='AzureBastionSubnet' publicIpAddressName='s4bastionpublicip' existingVNETName=$7 subnetAddressPrefix=$13
+az deployment group create  --resource-group $5 --template-uri "https://raw.githubusercontent.com/trstroem/one_touch_sap_deployment_on_azure/master/s4hana-infra-and-sw-e/azure-bastion-deploy.json" --parameters location=$6 bastionHostName='s4bastion' subnetName='AzureBastionSubnet' publicIpAddressName='s4bastionpublicip' existingVNETName=$7 subnetAddressPrefix=$13
 fi
-az deployment group create  --resource-group $5 --template-uri "https://raw.githubusercontent.com/sanjeevkumar761/one_touch_sap_deployment_on_azure/master/s4hana-infra-and-sw-e/jumpbox-windows-deploy.json" --parameters adminUsername='juser' adminPassword='Hana@123456' windowsOSVersion='2016-Datacenter' vmSize='Standard_D2_v3' location=$6 vNetName=$7 subnetName=$8
+az deployment group create  --resource-group $5 --template-uri "https://raw.githubusercontent.com/trstroem/one_touch_sap_deployment_on_azure/master/s4hana-infra-and-sw-e/jumpbox-windows-deploy.json" --parameters adminUsername='juser' adminPassword='Hana@123456' windowsOSVersion='2016-Datacenter' vmSize='Standard_D2_v3' location=$6 vNetName=$7 subnetName=$8
 cd souvenir/scenarios/hana-single-node-full-e/dev/terraform
 terraform init
 terraform apply -var net_rg_name=$5 -var az_resource_group=$5 -var az_region=$6 -var az_domain_name=$5 -var vnet_name=$7 -var subnet_name=$8 -var private_ip_address_hdb=$11 -auto-approve
